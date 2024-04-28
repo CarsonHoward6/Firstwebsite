796172359c149d1d8bf63bee712c0a97ccf03c03
carsonhoward6 <165042279+CarsonHoward6@users.noreply.github.com>
2024-04-28 02:22:30 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function menuClick(option, items) {
 
+  thisid=option.id;
+
+  alldivs=document.getElementsByClassName("visible")
+
+  for (let data of alldivs) {
+    console.log(data.id)
+    document.getElementById(data.id).className="invisible";
+  }
+  document.getElementById(items).className="visible";
+}
